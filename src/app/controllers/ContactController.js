9db3b3582b47a67652dd49ad742bf9acf3461de3
Wayne Souto <wayne@gfsis.com.br>
2023-07-05 22:25:46 -0300

    initial commit

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,24 @@
+class ContactController {
+  index(request, response) {
+    // Listar todos os registros
+    response.send('teste');
+  }
+
+  show() {
+    // Obter UM registro
+  }
+
+  store() {
+    // Criar novo registro
+  }
+
+  update() {
+    // Editar um registro
+  }
+
+  delete() {
+    // Deletar um registro
+  }
+}
+
+module.exports = new ContactController();
